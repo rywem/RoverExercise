51393b26ce8915eb8c40958491734c12a2720cbd
Ryan <cybervoid41@gmail.com>
2020-11-14 11:19:44 -0800

    ValidatePositionTests added.

---
+++ Rover.Solution/Rover.UnitTests/Objects/GridTests/ValidatePositionTests.cs
@@ -0,0 +1,69 @@
+﻿using Rover.ConsoleApplication.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Rover.UnitTests.Objects.GridTests
+{
+    public class ValidatePositionTests
+    {
+        [Fact]
+        public void ValidatePosition_WherePositionIsValid()
+        {
+            Grid grid = new Grid(5, 5);
+            Position position = new Position(1, 2, "N");
+
+            Assert.True(grid.ValidatePosition(position));
+        }
+
+        [Fact]
+        public void ValidatePosition_WherePositionIsValid_OnBorder()
+        {
+            Grid grid = new Grid(5, 5);
+            Position position = new Position(5, 5, "N");
+
+            Assert.True(grid.ValidatePosition(position));
+        }
+
+        [Fact]
+        public void ValidatePosition_WhereXPositionIsInvalid_AboveUpperBounds()
+        {
+            Grid grid = new Grid(5, 5);
+            Position position = new Position(10, 2, "N");
+
+            var ex = Assert.Throws<Exception>(() => grid.ValidatePosition(position));
+            Assert.Equal("Rover out of bounds.", ex.Message);
+        }
+
+        [Fact]
+        public void ValidatePosition_WhereXPositionIsInvalid_BelowLowerBounds()
+        {
+            Grid grid = new Grid(5, 5);
+            Position position = new Position(-1, 2, "N");
+
+            var ex = Assert.Throws<Exception>(() => grid.ValidatePosition(position));
+            Assert.Equal("Rover out of bounds.", ex.Message);
+        }
+
+        [Fact]
+        public void ValidatePosition_WhereYPositionIsInvalid_AboveUpperBounds()
+        {
+            Grid grid = new Grid(5, 5);
+            Position position = new Position(1, 10, "N");
+
+            var ex = Assert.Throws<Exception>(() => grid.ValidatePosition(position));
+            Assert.Equal("Rover out of bounds.", ex.Message);
+        }
+
+        [Fact]
+        public void ValidatePosition_WhereYPositionIsInvalid_BelowLowerBounds()
+        {
+            Grid grid = new Grid(5, 5);
+            Position position = new Position(2, -2, "N");
+
+            var ex = Assert.Throws<Exception>(() => grid.ValidatePosition(position));
+            Assert.Equal("Rover out of bounds.", ex.Message);
+        }
+    }
+}
